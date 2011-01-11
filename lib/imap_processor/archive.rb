6a8bf4bb3b56df8959933aab74c8d762bdc7a361
Ryan Davis <ryand@zenspider.com>
2011-01-10 16:10:01 -0800

    + Extended imap_archive to archive multiple months per box, as necessary. Allowing easy archiving of big mailboxes [git-p4: depot-paths = "//src/imap_processor/dev/": change = 6122]

--- lib/imap_processor/archive.rb
+++ lib/imap_processor/archive.rb
@@ -1,4 +1,5 @@
 require 'imap_processor'
+require "time"
 
 ##
 # Archives old mail on IMAP server by moving it to dated mailboxen.
@@ -65,25 +66,42 @@ imap_archive archives old mail on IMAP server by moving it to dated mailboxen.
 
   def run
     @boxes.each do |mailbox|
-      destination = "#{mailbox}#{sep}#{last_month}"
-
       log "SELECT #{mailbox}"
       response = imap.select mailbox
 
-      search = make_search
+      uids_by_date = self.uids_by_date
 
-      log "SEARCH #{search.join ' '}"
-      uids = imap.search search
+      next if uids_by_date.empty?
 
-      next if uids.empty?
+      uids_by_date.sort.each do |date, uids|
+        next if uids.empty?
+        destination = "#{mailbox}#{sep}%4d-%02d" % date
+        puts "#{destination}:"
+        puts
+        show_messages uids
+        move_messages uids, destination, false if move
+      end
 
-      puts "#{uids.length} messages in #{mailbox}#{list ? ':' : ''}"
-
-      show_messages uids
-
-      move_messages uids, destination if move
+      log "EXPUNGE"
+      imap.expunge
     end
   end
 
-end
+  def uids_by_date
+    search = make_search
+    log "SEARCH #{search.join ' '}"
+    uids = imap.search search
 
+    payload = imap.fetch(uids, 'BODY.PEEK[HEADER.FIELDS (DATE)]')
+
+    mail = Hash[uids.zip(payload).map { |uid, m|
+      date = m.attr["BODY[HEADER.FIELDS (DATE)]"].strip.split(/:\s*/, 2).last
+      [uid, Time.parse(date)]
+    }]
+
+    mail.keys.group_by { |uid|
+      date = mail[uid]
+      [date.year, date.month]
+    }
+  end
+end
