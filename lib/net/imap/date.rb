d658e62b71a1ac16551517c75d7848dfd9e4769b
Ryan Davis <ryand-ruby@zenspider.com>
2022-12-09 11:08:02 -0800

    + Added Time.imapdate(s), Time.imapdatetime(s), and #yyyy_mm. [git-p4: depot-paths = "//src/imap_processor/dev/": change = 13609]

--- lib/net/imap/date.rb
+++ lib/net/imap/date.rb
@@ -1,11 +1,28 @@
+require "time"
 
-class Time
+class Time                           # :nodoc:
+  IMAPDATE     = "%d-%b-%Y"          # :nodoc:
+  IMAPDATETIME = "%d-%b-%Y %H:%M %Z" # :nodoc:
+
+  ##
+  # Parse an IMAP date formatted string into a Time.
+
+  def self.imapdate str
+    Time.strptime str, IMAPDATE
+  end
+
+  ##
+  # Parse an IMAP datetime formatted string into a Time.
+
+  def self.imapdatetime str
+    Time.strptime str, IMAPDATETIME
+  end
 
   ##
   # Formats this Time as an IMAP-style date.
 
   def imapdate
-    strftime '%d-%b-%Y'
+    strftime IMAPDATE
   end
 
   ##
@@ -17,8 +34,13 @@ class Time
   # Go Mr. Leatherpants!
 
   def imapdatetime
-    strftime '%d-%b-%Y %H:%M %Z'
+    strftime IMAPDATETIME
   end
 
-end
+  ##
+  # Format a date into YYYY-MM, common for mailbox extensions.
 
+  def yyyy_mm
+    strftime("%Y-%m")
+  end
+end
